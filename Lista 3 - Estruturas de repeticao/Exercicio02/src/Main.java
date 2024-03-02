befee6a5242411180e37e5289d1bfc0f73d6e66c
unknown <gabrielfariasilva10@hotmail.com>
2024-03-02 00:45:13 -0300

    do while alternative solution added to ex 2

--- Lista 3 - Estruturas de repeticao/Exercicio02/src/Main.java
+++ Lista 3 - Estruturas de repeticao/Exercicio02/src/Main.java
@@ -3,10 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int n1, j = 0, big = 0, sml = 0, total_n = 5; //j = variável de contagem da primeira exec do 'for'
+        int n1, i = 0, j = 0, big = 0, sml = 0, total_n = 5; //j = variável de contagem da primeira exec do 'for'
 
         Scanner in = new Scanner(System.in);
 
+        do {
+            System.out.println("Digite um número inteiro: ");
+            n1 = in.nextInt();
+
+            if(j == 0){
+                big = n1;
+                sml = n1;
+                j++;
+            }
+
+            if(n1 > big){ //O número que entra é maior que o número da variável MAIOR?
+                big = n1;
+            }
+            else if(n1 < sml){ //Senão, é menor que o número da variável MENOR?
+                sml = n1;
+            }
+
+            i++;
+        } while(i < total_n);
+
+        System.out.println("Maior número = " + big + " e menor número = " + sml);
+
+        /* Utilizando 'for' no lugar de 'do while'
         System.out.println("Digite cinco números inteiros, separados por espaço: ");
 
         for (int i = 0; i < total_n; i++){
@@ -27,6 +50,6 @@ public class Main {
         }
 
         System.out.println("Maior número = " + big + " e menor número = " + sml);
-
+         */
     }
 }
