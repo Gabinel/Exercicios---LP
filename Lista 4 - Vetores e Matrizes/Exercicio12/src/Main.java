f1fbe1da27778d852641438a948d1f2d2cdff955
fariafariafaria <gabrielfariasilva10@hotmail.com>
2024-03-13 14:03:11 -0300

    ex12 fixed

--- Lista 4 - Vetores e Matrizes/Exercicio12/src/Main.java
+++ Lista 4 - Vetores e Matrizes/Exercicio12/src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
 
         int [] [] matrix = new int [3] [3];
-        //int [] [] detMatrix = new int [3] [5];
         int detFirst = 0, detSec = 0, mult = 1;
 
         Scanner in = new Scanner(System.in);
@@ -16,57 +16,65 @@ public class Main {
             }
         }
 
-        /*
-        //Completa 'matrix' com a primeira e segunda coluna duplicadas, em uma nova matriz 'detMatrix'
-        for (int k = 0; k < detMatrix.length; k++){
-            for (int l = 0; l < detMatrix[0].length; l++){
-                detMatrix[k][l] = in.nextInt();
-
-                if (l >= 3){
-                    detMatrix[k][l] = matrix[k][l-3];
-                }
-            }
-        }
-         */
-
         for (int k = 0; k < matrix.length; k++){
+
+            /*Para multiplicação dos valores das diagonais, é usado a fórmula geral
+            'mult = matrix[l][l + k] * mult'
+            As excessões (valores duplicados da matriz) estão incluídos nos 'if - else'
+             */
             for (int l = 0; l < matrix[0].length; l++){
-                if (l > 1 && k == 1){
-                    mult = matrix[0][l] * mult;
-                } else if (l >= 1 && k == 2){
-                    mult = matrix[0][l] * mult;
-                } else {
+
+                if (l == 2 && k == 1){
+                    mult = matrix[l][0] * mult;
+                }
+                else if (l >= 1 && k == 2){
+                    mult = matrix[l][l - 1] * mult;
+                }
+                else {
                     mult = matrix[l][l + k] * mult;
+
                 }
 
-                detFirst = mult + detFirst;
-
             }
+
+            detFirst = mult + detFirst;
+
+            //Reseta o 'mult' para que a próxima diagonal seja calculada
+            mult = 1;
+
         }
 
-        mult = 1;
+        /*
+        Aqui é usado a mesma linha de raciocínio do exercício 9: para calcular a segunda diagonal
+        da matriz é usada a fórmula '|n - (comprimentoDaMatriz - 1) + m|
 
+        Exemplo: n = 0, m = 0:
+        |0 - (3 - 1) + 0| => |-3| => -(-3) => 3
+
+        Os valores duplicados da matriz estão incluídos nos 'if - else'
+        */
         for (int m = 0; m < matrix.length; m++){
-            for (int n = (matrix.length - 1); n >= 0; n--){
-                if (n == 2 && m == 1){
+
+            for (int n = 0; n < matrix[0].length; n++){
+
+                if (n == 0 && m == 1){
                     mult = matrix[0][0] * mult;
-                } else if (n >= 1 && m == 2){
-                    mult = matrix[0][n - 1] * mult;
-                } else {
-                    mult = matrix[n][n + m] * mult;
+                }
+                else if (n <= 1 && m == 2){
+                    mult = matrix[n][m - (n + 1)] * mult;
+                }
+                else {
+                    mult = matrix[n][Math.abs(n - (matrix[0].length- 1)) + m] * mult;
                 }
 
-                detSec = mult + detSec;
-
             }
+
+            detSec = mult + detSec;
+            mult = 1;
+
         }
 
         System.out.println("A determinante da matriz é: " + (detFirst - detSec));
 
     }
-
-
-
-
-
 }
